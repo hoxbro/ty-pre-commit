51b83cb96958c444dd16fc35e59a26178a6c27a2
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-17 10:14:11 +0100

    first init

---
+++ README.md
@@ -0,0 +1,16 @@
+# TY pre-commit
+
+For Taplo: see https://github.com/astral-sh/ty
+
+For pre-commit: see https://github.com/pre-commit/pre-commit
+
+## Using TY with pre-commit
+
+Add this to your `.pre-commit-config.yaml`:
+
+```yaml
+- repo: https://github.com/hoxbro/ty-pre-commit
+  rev: v0.0.2
+  hooks:
+    - id: taplo
+```
---
+++ update.py
@@ -0,0 +1,59 @@
+# /// script
+# dependencies = ["httpx"]
+# ///
+
+import re
+from subprocess import run
+from textwrap import dedent
+
+import httpx
+
+URL = "https://pypi.org/pypi/ty/json"
+HOOKFILE = ".pre-commit-hooks.yaml"
+READMEFILE = "README.md"
+
+
+def main():
+    resp = httpx.get(URL).raise_for_status()
+    version = resp.json()["info"]["version"]
+
+    hook = f"""\
+    - id: ty
+      name: ty
+      description: ""
+      entry: ty check
+      language: python
+      types: [python]
+      args: []
+      require_serial: false
+      additional_dependencies: ["ty=={version}"]
+      minimum_pre_commit_version: "0"
+      """
+    hook = dedent(hook)
+
+    with open(HOOKFILE) as f:
+        cur_hook = f.read()
+
+    if hook == cur_hook:
+        return
+
+    with open(HOOKFILE, "w") as f:
+        f.write(hook)
+
+    with open(READMEFILE) as f:
+        readme = f.read()
+
+    readme = re.sub("rev: v.*", f"rev: v{version}", readme)
+
+    with open(READMEFILE, "w") as f:
+        f.write(readme)
+
+    run(["git", "add", HOOKFILE, READMEFILE], check=True)
+    run(["git", "commit", "-m", f"Update to version {version}"], check=True)
+    run(["git", "tag", f"v{version}"], check=True)
+    run(["git", "push", "origin"], check=True)
+    run(["git", "push", "origin", "--tags"], check=True)
+
+
+if __name__ == "__main__":
+    main()
